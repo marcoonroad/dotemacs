a150b80184a4d0eec61d087bbdd6bb37e2f0f3cd
Quildreen Motta <quildreen@gmail.com>
2011-03-24 15:14:32 -0300

    Basic emms configuration.

---
+++ sorella/emms.el
@@ -0,0 +1,9 @@
+;; ---------------------------------------------------------------------
+;; Emms configuration
+;; ---------------------------------------------------------------------
+
+(emms-devel)
+(emms-default-players)
+
+(setq emms-source-file-default-directory "~/Music/")
+(setq emms-info-asynchronously t)
