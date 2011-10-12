cb8ecaf52d92aa9ee61b5e8bce651530d8d3156a
Quildreen Motta <quildreen@gmail.com>
2011-10-12 18:36:34 -0300

    Includes parfait definitions for Java.

--- sorella/parfait.el
+++ sorella/parfait.el
@@ -13,7 +13,22 @@
      ("||"             . "∨ ")))
   (parfait-mode t))
 
+(defun parfait-java ()
+  (interactive)
+  (make-local-variable 'parfait-symtable)
+  (make-local-variable 'tab-width)
+  (setq tab-width         80)
+  (setq parfait-symtable '())
+
+  (parfait-add-symbols
+   '(("\\<this\\." . "@")
+     ("\\>_\\<" . "-")
+     ("[{};]+[[:blank:]]*$" . "	\\")))
+  (c-toggle-electric-state -1)
+  (parfait-mode             t))
+
 (add-hook 'js2-mode-hook        'parfait-javascript)
 (add-hook 'js3-mode-hook        'parfait-javascript)
 (add-hook 'javascript-mode-hook 'parfait-javascript)
+(add-hook 'java-mode-hook       'parfait-java)
           
