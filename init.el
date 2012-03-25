1379b82621f0ba84459677e084f47b5481da5efc
Quildreen Motta <quildreen@gmail.com>
2012-03-25 13:21:30 -0300

    Adds new initialisation for profiles.

---
+++ init.el
@@ -0,0 +1,30 @@
+;; Based on Emacs Prelude's prelude-package.el
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(package-initialize)
+
+
+(defun sa/packages-install (package-list)
+  "Installs and updates all of the packages listed."
+  (message "Refreshing the packages...")
+  (package-refresh-contents)
+  (dolist (package package-list)
+    (when (not (package-installed-p package))
+      (package-install package)))
+  (message "Packages updated successfully."))
+
+
+(defmacro sa/use-packages (&rest package-list)
+  `(sa/packages-install '(,@package-list)))
+
+
+(defun sa/vendor-add (&rest modules)
+  (dolist (module modules)
+    (add-to-list 'load-path (concat "~/.emacs.d/vendor/" module))))
+
+
+;; Load the right profile
+(add-to-list 'load-path "~/.emacs.d")
+(add-to-list 'load-path "~/.emacs.d/profiles")
+(load "sorella/profile")
