3ac1fb68d0a3029e1f7319ad4badce3c75f03d2c
Quildreen "Sorella" Motta <quildreen@gmail.com>
2013-03-30 04:16:35 -0300

    Updates diff-hl when commiting Mercurial stuff on aHg.

--- profiles/sorella/versioning.el
+++ profiles/sorella/versioning.el
@@ -1,13 +1,18 @@
 ;;; versioning.el --- Packages that deal with VCS
 
 ;; ------------------------------------------------------------------------
+;; ahg: Deals with Mercurial repositories betterer
+;; ------------------------------------------------------------------------
+(require 'ahg)
+
+;; ------------------------------------------------------------------------
 ;; diff-hl: Highlight uncommited changes in the buffer
 ;; ------------------------------------------------------------------------
 (when (package-installed-p 'diff-hl)
   (require 'diff-hl)
-  (global-diff-hl-mode))
+  (global-diff-hl-mode)
+  
 
-;; ------------------------------------------------------------------------
-;; ahg: Deals with Mercurial repositories betterer
-;; ------------------------------------------------------------------------
-(require 'ahg)
+  (when (featurep 'ahg)
+    (defadvice ahg-status-maybe-refresh (after ahg-update-diff-hl activate)
+      (diff-hl-update))))
