5408ee113dc1ada2c8be1673a88ed5ea31f93522
Quildreen "Sorella" Motta <quildreen@gmail.com>
2013-03-29 21:56:44 -0300

    Moves LiveScript stuff to its own file.

--- profiles/sorella/programming/coffeescript.el
+++ profiles/sorella/programming/coffeescript.el
@@ -5,5 +5,3 @@
 
 (add-to-list 'auto-mode-alist '("\\.coffee$"  . coffee-mode))
 (add-to-list 'auto-mode-alist '("Cakefile$"   . coffee-mode))
-(add-to-list 'auto-mode-alist '("\\.ls$"      . coffee-mode))
-(add-to-list 'auto-mode-alist '("Slakefile$"  . coffee-mode))
